3f2791e6a8bd2f6dcc1fdd1e98a6ab40886ee6d9
michitzky <michitzky@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-11 13:20:15 +0000

    updated version numbers
    git-svn-id: https://plugins.svn.wordpress.org/mk-simple-backups/tags/0.4.4@891342 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mk-simple-backups.php
+++ mk-simple-backups.php
@@ -4,7 +4,7 @@
  * Plugin Name: mk Simple Backups
  * Plugin URI: http://wordpress.org/plugins/mk-simple-backups/
  * Description: Allows you to create simple backups on a dedicated page nested in the "Tools" Menu.
- * Version: 0.4.2
+ * Version: 0.4.4
  * Author: Michael Kühni
  * Author URI: http://michaelkuehni.ch
  * License: GPL2
